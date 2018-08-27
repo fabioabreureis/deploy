fe87cf8ad3faa9c832746a4dac2e59d25c143273
Ceph Brasil <contato@cephbr.com>
2018-08-27 08:45:51 -0300

    Revision
    Revision

--- resources/osd.sh
+++ resources/osd.sh
@@ -1,5 +1,7 @@
-sudo parted -s /dev/sdb mklabel gpt mkpart primary xfs 0% 100%
-sudo mkfs.xfs /dev/sdb -f
+sudo parted --script /dev/sdb mklabel gpt mkpart primary xfs 1MB 1500MB mkpart primary xfs 1501MB 3000MB mkpart primary xfs 3001MB 4500MB
+sudo mkfs.xfs /dev/sdb1 -f
+sudo mkfs.xfs /dev/sdb2 -f
+sudo mkfs.xfs /dev/sdb3 -f
 sudo parted -s /dev/sdc mklabel gpt mkpart primary xfs 0% 100%
 sudo mkfs.xfs /dev/sdc -f
 sudo parted -s /dev/sdd mklabel gpt mkpart primary xfs 0% 100%
