7351cc24ba1a35be8b1f60249d1fbd2a0b137dc4
Ceph Brasil <contato@cephbr.com>
2018-08-28 09:50:28 -0300

    Revision
    Revision

--- resources/osd.sh
+++ resources/osd.sh
@@ -1,6 +1,24 @@
-sudo parted -s /dev/sdb mklabel gpt mkpart primary xfs 0% 100%
-sudo mkfs.xfs /dev/sdb -f
-sudo parted -s /dev/sdc mklabel gpt mkpart primary xfs 0% 100%
-sudo mkfs.xfs /dev/sdc -f
-sudo parted -s /dev/sdd mklabel gpt mkpart primary xfs 0% 100%
-sudo mkfs.xfs /dev/sdd -f
+#!/bin/bash 
+
+case "$1" in
+
+filestore) 
+# journal 
+parted /dev/sdb --script -- mklabel gpt 
+parted --script /dev/sdb mkpart primary 0MB 2420MB 
+parted --script /dev/sdb mkpart primary 2421MB 4842MB 
+sgdisk --zap-all --clear --mbrtogpt -g -- /dev/sdb1
+sgdisk --zap-all --clear --mbrtogpt -g -- /dev/sdb2
+ceph-volume lvm zap /dev/sdb1
+ceph-volume lvm zap /dev/sdb2 
+
+# data 
+dd if=/dev/zero of=/dev/sdc  bs=1M count=1000
+dd if=/dev/zero of=/dev/sdd  bs=1M count=1000
+;;
+
+*)
+echo "Usage ./osd.sh filestore"
+;;
+
+esac 
